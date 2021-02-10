ee0be0b723152b61b7179cd2f986bfa72e873182
Ladder <ladderackroyd@gmail.com>
2021-02-10 20:27:43 +0000

    Corrected error in package.json and added empty react app

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'styles')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get("/", (req, res) => {
-    res.sendFile('UnderDevelopment.html', {root: __dirname});
-});
+// app.get("/", (req, res) => {
+//     res.sendFile('UnderDevelopment.html', {root: __dirname});
+// });
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`)
