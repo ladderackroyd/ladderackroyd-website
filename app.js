7e9ae98f05dff017580422ff56e2d9d2043092e1
Ladder <ladderackroyd@gmail.com>
2021-02-10 19:52:20 +0000

    Added under development page

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
-
+const path = require("path");
 const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, 'styles')));
+
 app.get("/", (req, res) => {
-    res.send("Something is being worked on by ladderackroyd...");
+    res.sendFile('UnderDevelopment.html', {root: __dirname});
 });
 
 app.listen(port, () => {
